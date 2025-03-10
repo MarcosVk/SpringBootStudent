39ed6f22607b5de26148bb194b54359bc261eb00
MarcosVk <86157537+MarcosVk@users.noreply.github.com>
2025-03-10 10:16:06 +0530

    Added column names

--- src/main/java/com/example/demo/Student/Student.java
+++ src/main/java/com/example/demo/Student/Student.java
@@ -6,11 +6,34 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.Period;
 
-@Entity
-@Table(name="student")
+@Entity(name="Student")
+@Table(
+		name="student",
+		uniqueConstraints = {
+				@UniqueConstraint(name="student_email_unique",columnNames = "email")
+		}
+)
 public class Student{
 
 @Id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @SequenceGenerator(
 	name="student_sequence",
 	sequenceName="student_sequence",
@@ -20,9 +43,29 @@ public class Student{
 	strategy=GenerationType.SEQUENCE,
 	generator="student_sequence"
 )
+@Column(
+		name = "id",
+		updatable = false,
+		nullable = false
+)
 private Long id;
+@Column(
+		name = "name",
+		columnDefinition = "TEXT",
+		nullable = false
+)
 private String name;
+	@Column(
+			name = "date",
+			nullable = false,
+			columnDefinition = "TEXT"
+	)
 private LocalDate date;
+	@Column(
+			name = "email",
+			nullable = false,
+			unique = true
+	)
 private String email;
 @Transient
 private Integer age;
